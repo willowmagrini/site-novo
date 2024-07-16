ceec9f094056038adc65ba35eb89abece635b5bb
willow <brmagrini@gmail.com>
2024-07-16 16:23:43 -0300

    altera script de criação de tema

--- dev-scripts/cria-tema.sh
+++ dev-scripts/cria-tema.sh
@@ -8,12 +8,12 @@ fi
 
 # Atribui os parâmetros às variáveis
 string_nova=$1
-mv themes/hacklab-theme $string_nova
+mv themes/hacklab-theme themes/$string_nova
 
 # Diretório atual
 diretorio=$(pwd)
 
 # Substitui as ocorrências da string antiga pela nova em todos os arquivos do diretório atual e subdiretórios
-find "$diretorio" -type f -exec sed -i "s/hacklab-theme/$string_nova/g" {} +
-
-echo "Substituição concluída com sucesso."
+find "$diretorio" -type f ! -path \".git\" -prune -name cria-tema.sh -exec sed -i "s/hacklab-theme/$string_nova/g" {} +
+rm style.css
+ln -s themes/$string_nova/style.css style.css
